497d85aabd2bd0f1733f794731866683a75826cd
arkneca <stojanpro@gmail.com>
2016-11-22 20:37:32 +0100

    Add and setup act_as_votable gem

--- raddit/config/routes.rb
+++ raddit/config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'links#index'
-  resources :links
+  root "links#index"
+  resources :links do
+    member do
+      put "like",    to: "links#upvote"
+      put "dislike", to: "links#downvote"
+    end
+  end
 end
