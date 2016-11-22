5ba6b70ce1d8204e210f5b59c74b7df2bc71e056
arkneca <stojanpro@gmail.com>
2016-11-22 21:18:00 +0100

    Add Comments

--- raddit/config/routes.rb
+++ raddit/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :comments
   devise_for :users
   root "links#index"
   resources :links do
@@ -6,5 +7,6 @@ Rails.application.routes.draw do
       put "like",    to: "links#upvote"
       put "dislike", to: "links#downvote"
     end
+    resources :comments
   end
 end
