e01f48be6a86690d53c8dc753db9701b8bf84587
psto <piotrstojan@gmail.com>
2016-12-07 18:26:30 +0100

    Add posts

---
+++ blog/app/controllers/posts_controller.rb
@@ -0,0 +1,26 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.order(created_at: :desc)
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to @post
+  end
+
+
+  private
+
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
+end
