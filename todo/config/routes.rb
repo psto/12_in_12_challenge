6c12cc3afcaac5cd327b1b22181483d08627d9ab
psto <piotrstojan@gmail.com>
2016-12-09 09:13:32 +0100

    Added Todo List and Todo Items

--- todo/config/routes.rb
+++ todo/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'todo_lists#index'
+
+  resources :todo_lists do
+    resources :todo_items
+  end
 end
