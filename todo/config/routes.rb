68090d52ec27e995cf6ede8ce7c586b0bf7a1cb3
psto <piotrstojan@gmail.com>
2016-12-09 11:00:59 +0100

    Mark as complete ability

--- todo/config/routes.rb
+++ todo/config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   root 'todo_lists#index'
 
   resources :todo_lists do
-    resources :todo_items
+    resources :todo_items do
+      member do
+        patch :complete
+      end
+    end
   end
 end
