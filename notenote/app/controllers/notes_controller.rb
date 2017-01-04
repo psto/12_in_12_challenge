a2c58cb1a2a5d11a19a16dccc17dcbe090ec83bd
psto <piotrstojan@gmail.com>
2017-01-04 22:25:27 +0100

    Add Simple_Form & Note Views

--- notenote/app/controllers/notes_controller.rb
+++ notenote/app/controllers/notes_controller.rb
@@ -1,14 +1,25 @@
 class NotesController < ApplicationController
+  before_action :find_note, only: [:show, :edit, :update, :destroy]
+
   def index
+    @notes = Note.all.order("created_at DESC")
   end
 
   def show
   end
 
   def new
+    @note = Note.new
   end
 
   def create
+    @note = Note.new(note_params)
+
+    if @note.save
+      redirect_to @note
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -23,8 +34,10 @@ class NotesController < ApplicationController
   private
 
   def find_note
+    @note = Note.find(params[:id])
   end
 
   def note_params
+    params.require(:note).permit(:title, :content)
   end
 end
